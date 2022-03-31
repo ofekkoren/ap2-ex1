e5c737f0752288da5f2161540446e80bf7144dad
Ofek Koren <ofekkoren1@gmail.com>
2022-03-31 17:08:34 +0300

    added background

--- src/Logging.js
+++ src/Logging.js
@@ -2,21 +2,18 @@ import './Logging.css';
 
 function Logging() {
   return (
-    <form>
-      <div className="row g-2 align-items-center">
+    <form className="myform">
+      <div className="row center-user">
         <div className="col-auto">
           <label className="form-label">Username</label>
         </div>
         <div className="col-md-3">
           <input type="text" placeholder="Enter your username" className="form-control" required></input>
         </div>
-
       </div>
-
       <div className="mb-3">
         <button type="submit" className="btn btn-primary">Submit</button>
       </div>
-
     </form>
   );
 }
