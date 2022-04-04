9571bd0754ba40691607b94f356139f994799a1f
TOMER-77 <tomer-77@windowslive.com>
2022-04-05 02:02:44 +0300

    added right part of the chat screen

--- src/chatScreen/ChatScreen.js
+++ src/chatScreen/ChatScreen.js
@@ -1,6 +1,7 @@
 import './ChatScreen.css';
-import { useState } from "react";
+import {useState} from "react";
 import LeftScreen from './LeftScreen';
+import RightScreen from "./RightScreen";
 
 function ChatScreen(user) {
     let [currentConversation, setCurrentConversation] = useState("");
@@ -8,13 +9,8 @@ function ChatScreen(user) {
     return (
         <div className="container-chat-screen justify-content-center">
             <div className="inner-chat-cube">
-                <LeftScreen />
-                <div className="col rightScreen">
-                    <div className="topLine">
-                        kkk
-                    </div>
-                    right
-                </div>
+                <LeftScreen/>
+                <RightScreen/>
             </div>
         </div>
     );
---
+++ src/chatScreen/RightScreen.js
@@ -0,0 +1,16 @@
+import './ChatScreen.css';
+import "./RightScreen.css"
+
+function RightScreen() {
+    return (
+        <div className="col rightScreen">
+            <div className="topLine">
+                <img src={require('../images/userImages/boy-image.png')} className="rounded float-start top-profile-image" ></img>
+                <h5>User Nickname</h5>
+            </div>
+            right chat
+        </div>
+    );
+}
+
+export default RightScreen;
