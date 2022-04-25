fd568b9fcff91f59d8d110ab3e6e6ab5d3953273
TOMER-77 <tomer-77@windowslive.com>
2022-04-25 18:26:53 +0300

    -

--- src/db/UsersDataBase.js
+++ src/db/UsersDataBase.js
@@ -3,7 +3,7 @@ import conversations from '../db/Conversations';
 let users = [{username: 'Ofek Koren', nickname: 'Ofekkoren', password: '123456K', image: "/images/userImages/girl-image.jpg", chats:[conversations[0], conversations[1]]},
 {username: 'Tomer Eligayev', nickname: 'Tomer-77', password: '123456E', image: "/images/userImages/boy-image.png", chats:[conversations[0], conversations[2], conversations[3]]},
 {username: 'Avi Cohen', nickname: 'Avi', password: '123456C', image: "/images/userImages/boy-image.png", chats: [conversations[1],conversations[3]]},
-{username: 'Shir Levi', nickname: 'Shirus', password: '123456L', image: "/images/userImages/b.jpg", chats: []},
-{username: 'Moti Luhim', nickname: 'Moti Luhim', password: '123456L', image: "/images/userImages/h.jpg", chats:[conversations[2]]}];
+{username: 'Shir Levi', nickname: 'Shirus', password: '123456L', image: "/images/userImages/girl-image.jpg", chats: []},
+{username: 'Moti Luhim', nickname: 'Moti Luhim', password: '123456L', image: "/images/userImages/boy-image.png", chats:[conversations[2]]}];
 
 export default users;
