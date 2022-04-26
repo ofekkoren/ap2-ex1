c9606fe0b428eec5c5eacb58fc2279c09ce51bfc
Ofek Koren <ofekkoren1@gmail.com>
2022-04-26 19:24:24 +0300

    added pictures

--- src/db/Conversations.js
+++ src/db/Conversations.js
@@ -3,12 +3,14 @@ let conversations = [
             {username: 'Tomer Eligayev', nickname: 'Tomer-77', image: "/images/userImages/boy-image.png"}],
         messages:[{type:"text", content: "Hello, how are you?", createdAt: '2018-01-01T00:00:00.000Z', sender: "Ofek Koren"},
             {type:"text", content: "I'm fine, thanks", createdAt: '2020-10-10T12:48:00.000Z', sender: "Tomer Eligayev"},
-            {type:"text", content: "Have a nice day.", createdAt: '2020-10-10T12:48:00.000Z', sender: "Tomer Eligayev"}]},
+            {type:"text", content: "Have a nice day.", createdAt: '2020-10-10T12:48:00.000Z', sender: "Tomer Eligayev"},
+            {type:"image", content: "/mediaMessages/happyIndependenceDay.jpg", createdAt: '2020-10-10T12:48:00.000Z', sender: "Ofek Koren"}]},
     {users: [{username: "Ofek Koren", nickname: "Ofekkoren", image: "/images/userImages/girl-image.jpg"},
             {username: 'Avi Cohen', nickname: 'Avi', image: "/images/userImages/boy-image.png"}],
         messages:[{type:"text", content: "Heyyy, want to eat icecream?", createdAt: '2020-10-10T12:48:00.000Z', sender: "Avi Cohen"},
             {type:"text", content: "I'm fine, thanks", createdAt: '2020-10-10T12:48:00.000Z', sender: "Ofek Koren"},
             {type:"text", content: "Do I know you?", createdAt: '2020-10-10T12:48:00.000Z', sender: "Ofek Koren"},
+            {type:"image", content: "/mediaMessages/iceCream.jpg", createdAt: '2020-10-10T12:48:00.000Z', sender: "Avi Cohen"},
             {type:"text", content: "No, but we can :)", createdAt: '2020-10-10T12:48:00.000Z', sender: "Avi Cohen"}]},
     {users: [{username: 'Tomer Eligayev', nickname: 'Tomer-77', image: "/images/userImages/boy-image.png"},
             {username: 'Moti Luhim', nickname: 'Moti Luhim', image: "/images/userImages/boy-image.png"}],
@@ -38,9 +40,6 @@ let conversations = [
             {type:"text", content: "Yess , apparently fuel made of eggplants makes a great spaceships fuel", createdAt: '2022-04-10T12:48:00.000Z', sender: "Moti Luhim"},
             {type:"text", content: "wowww this is unbelievable!", createdAt: '2022-04-10T12:48:00.000Z', sender: "Shir Levi"},
             {type:"image", content: "/mediaMessages//spaceEggplantImage.png", createdAt: '2022-04-10T12:48:00.000Z', sender: "Moti Luhim"},
-
-
-
         ]}
 ]
 
